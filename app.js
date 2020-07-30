640d864f7f355698b0b7d836eac497827cb79962
Alex Sanschagrin <asanscha@gmail.com>
2020-07-29 17:01:40 -0700

    added the left edge of the game

--- app.js
+++ app.js
@@ -86,7 +86,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //define the left edge of the game
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
 
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition +=1
+        }
+
+        draw()
+    }
 
 
 
